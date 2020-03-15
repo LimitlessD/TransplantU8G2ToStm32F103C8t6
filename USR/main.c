e33e0215f3183581f8cb54d2e092a55c9c2ea762
limitlessD <dengyun2012@gmail.com>
2020-03-16 03:00:12 +0800

    add sysinit.c

--- USR/main.c
+++ USR/main.c
@@ -5,12 +5,6 @@ void GPIO_Configuration(void);
 void USART_Configuration(void);
 u8g2_t InitNokia5110(void);
 
-int fputc(int ch,FILE *f)
-{
-	USART_SendData(USART1,(u8)ch);
-	while(USART_GetFlagStatus(USART1,USART_FLAG_TXE)==RESET);
-	return ch;
-}
 
 int main(void)
 {
@@ -34,54 +28,3 @@ int main(void)
 		 delay_ms(50);
    }	
 }
-
-void RCC_Configuration(void)
-{
-    SystemInit();//72m
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO,ENABLE);
-	GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable,ENABLE);//要先开时钟，再重映射；这句表示关闭jtag，使能swd。  
-}
-
-void GPIO_Configuration(void)
-{
-  GPIO_InitTypeDef GPIO_InitStructure;	
-	//LED
-	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_9;//TX
-	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_AF_PP;
-	GPIO_Init(GPIOA,&GPIO_InitStructure);
-
-	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_10;//RX
-	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_IN_FLOATING;
-	GPIO_Init(GPIOA,&GPIO_InitStructure);
-	
-	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_3|GPIO_Pin_4|GPIO_Pin_5|GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8;
-	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_Out_PP;
-	GPIO_Init(GPIOB,&GPIO_InitStructure);
-}
-
-
-void USART_Configuration(void)
-{
-  USART_InitTypeDef  USART_InitStructure;
-
-	USART_InitStructure.USART_BaudRate=9600;
-	USART_InitStructure.USART_WordLength=USART_WordLength_8b;
-	USART_InitStructure.USART_StopBits=USART_StopBits_1;
-	USART_InitStructure.USART_Parity=USART_Parity_No;
-	USART_InitStructure.USART_HardwareFlowControl=USART_HardwareFlowControl_None;
-	USART_InitStructure.USART_Mode=USART_Mode_Rx|USART_Mode_Tx;
-
-	USART_Init(USART1,&USART_InitStructure);
-	USART_ITConfig(USART1,USART_IT_RXNE,ENABLE);
-	USART_Cmd(USART1,ENABLE);
-	USART_ClearFlag(USART1,USART_FLAG_TC);
-}
-
-
-
-
